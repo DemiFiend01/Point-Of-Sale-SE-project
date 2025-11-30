8afbe108a5b645f3f82662c880cef29a9ff08a23
DemiFiend01 <maggie.mr1606@gmail.com>
2025-11-30 20:04:59 +0100

    Stopped tracking

--- Prototype_1_Django/POS_System/POS_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('POS_app/', views.POS_app, name='POS_app'),
-]
--- Prototype_1_Django/POS_System/POS_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def POS_app(request):
-    return HttpResponse("Hello world!")
